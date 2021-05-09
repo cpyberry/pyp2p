4b0e75ab7032d0ba5ae200d5eacbcde3c8243c97
cpyberry <cpyberry222@gmail.com>
2021-05-09 13:46:26 +0900

    feat: add a class Pyp2p
    send and receive various objects

---
+++ pyp2p/pyp2p.py
@@ -0,0 +1,14 @@
+import socket
+import struct
+from typing import Any
+from .message import CreateMessage, ParseMessage
+
+
+ENDIANNESS = ">"
+
+
+class Pyp2p:
+	"""send and receive various objects
+	"""
+
+	pass
