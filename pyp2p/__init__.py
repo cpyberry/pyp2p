ec39f9e2edabff88b8f94aab10c9c9e35d7399ba
cpyberry <cpyberry222@gmail.com>
2021-05-09 14:13:42 +0900

    feat: add __init__.py

---
+++ pyp2p/__init__.py
@@ -0,0 +1,9 @@
+from .message import *
+from .pyp2p import *
+
+
+__copyright__ = "Copyright 2021 cpyberry"
+__url__ = "https://github.com/cpyberry/pyp2p"
+__license__ = "Apache-2.0 License"
+__author__ = "cpyberry"
+__author_email__ = "cpyberry222@gmail.com"
