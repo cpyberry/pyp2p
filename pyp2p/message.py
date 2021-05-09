10b52b2d1a73ab303f8aa7250dafe7d6ea3381b4
cpyberry <cpyberry222@gmail.com>
2021-05-09 10:24:28 +0900

    feat: add a function prepend_message_length
    add message length

--- pyp2p/message.py
+++ pyp2p/message.py
@@ -12,3 +12,16 @@ class CreateMessage:
 			format_char (str, optional): Format of struct module for the message length to insert at the beginning. Defaults to "I".
 		"""
 		self.format = ENDIANNESS + format_char
+
+	def prepend_message_length(self, data: bytes) -> bytes:
+		"""add message length
+
+		Args:
+			data (bytes): data to which you want to add the message length
+
+		Returns:
+			bytes: data with message length prepend
+		"""
+		length = len(data)
+		message = struct.pack(self.format, length) + data
+		return message
